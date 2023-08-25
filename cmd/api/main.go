f5ff127a5532e4c4ce3d927bf3643c1ffae65e68
Vincent Marger <vincent.marger@gmail.com>
2023-08-25 16:51:33 -0400

    API movie insert

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"time"
 
+	"example/myimdb/internal/data"
+
 	_ "github.com/lib/pq"
 )
 
@@ -29,6 +31,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -51,9 +54,12 @@ func main() {
 	defer db.Close()
 	logger.Printf("database connection pool established")
 
+
+
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
