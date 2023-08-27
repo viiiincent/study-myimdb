2542158ce57630a931b7f7ac41b9d6686bb236dc
Vincent Marger <vincent.marger@gmail.com>
2023-08-27 19:36:11 -0400

    add api sort filter

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"strings"
+
 	"example/myimdb/internal/validator"
 )
 
@@ -17,4 +19,21 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
