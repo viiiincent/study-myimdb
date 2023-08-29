0e9f0455d38266d46f6ea7032679de4179e5670e
Vincent Marger <vincent.marger@gmail.com>
2023-08-28 21:46:53 -0400

    Add mailer and send email on user registration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 
 	"example/myimdb/internal/data"
 	"example/myimdb/internal/jsonlog"
+	"example/myimdb/internal/mailer"
 
 	_ "github.com/lib/pq"
 )
@@ -29,12 +30,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -52,6 +61,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps,   "limiter-rps",     2,    "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst,     "limiter-burst",   4,    "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled,  "limiter-enabled", true, "Enable rate limiter")
+	// mailer
+	flag.StringVar(&cfg.smtp.host,     "smtp-host",     "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port,        "smtp-port",     25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "d518ae93895b1b", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "a6b80fa828bfc5", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender,   "smtp-sender",   "MyIMDB <no-reply@myimdb.lol>", "SMTP sender")
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -67,6 +82,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
