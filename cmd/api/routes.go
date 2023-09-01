ad32e2e0203fe77e72d28287430a41701998119b
Vincent Marger <vincent.marger@gmail.com>
2023-09-01 18:36:49 -0400

    Add expvar endpoint and custom metrics

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -12,17 +13,19 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet,    "/v1/healthcheck", app.healthcheckHandler)
-
+	// movies
 	router.HandlerFunc(http.MethodGet,    "/v1/movies",      app.requirePermission("movies:read",  app.listMoviesHandler))
 	router.HandlerFunc(http.MethodPost,   "/v1/movies",      app.requirePermission("movies:write", app.createMovieHandler))
 	router.HandlerFunc(http.MethodGet,    "/v1/movies/:id",  app.requirePermission("movies:read",  app.showMovieHandler))
 	router.HandlerFunc(http.MethodPatch,  "/v1/movies/:id",  app.requirePermission("movies:write", app.updateMovieHandler))
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id",  app.requirePermission("movies:write", app.deleteMovieHandler))
-
+	// users
 	router.HandlerFunc(http.MethodPost,   "/v1/users",          app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut,    "/v1/users/activate", app.activateUserHandler)
-
+	// tokens
 	router.HandlerFunc(http.MethodPost,   "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	// metrics
+	router.Handler(http.MethodGet,    "/debug/vars", expvar.Handler())
 
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
 }
